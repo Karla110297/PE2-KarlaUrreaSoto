de93f1b9f0bc0cb9b75fb1e65fae2f40647c9a68
Karla110297 <dinorakarla@hotmail.com>
2018-10-14 17:46:58 -0700

    Add project files.

---
+++ PE2-KarlaUrreaSoto/Operaciones.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE2_KarlaUrreaSoto
+{
+    class Operaciones
+    {
+
+        public double[] MaxMin(double[] arreglo, int n, double[] Soluciones)
+        {
+
+            if (n >= 0)
+            {
+                if (arreglo[n] >= Soluciones[0])
+                {
+                    Soluciones[0] = arreglo[n];
+                }
+                if (n==(arreglo.Length-1)||arreglo[n] <= Soluciones[1])
+                {
+                    Soluciones[1] = arreglo[n];
+                }
+                MaxMin(arreglo, n - 1, Soluciones);
+            }
+            return Soluciones;
+        }
+        
+        public double[] Inversion(double[] arreglo, int n, double[] arregloinv, int i)
+        {
+
+            if (n >= 0)
+            {
+                arregloinv[i] = arreglo[n];
+
+                Inversion(arreglo, n - 1, arregloinv, i + 1);
+            }
+            return arregloinv;
+        }
+    }
+}
---
+++ PE2-KarlaUrreaSoto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE2_KarlaUrreaSoto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones f = new Operaciones();
+
+            Console.WriteLine("Tamaño de la sucesión");
+            int tamanioSucesion = Int32.Parse(Console.ReadLine());
+
+            double[] sucesion = new double[tamanioSucesion];
+            double[] sucesionInv = new double[tamanioSucesion];
+
+            double[] mayorMenor = new double[2];
+
+            for (int i = 0; i < tamanioSucesion; i++)
+            {
+                Console.WriteLine("Numero  " + (i + 1));
+                sucesion[i] = double.Parse(Console.ReadLine());
+
+            }
+
+            mayorMenor = f.MaxMin(sucesion, sucesion.Length - 1, mayorMenor);
+            Console.WriteLine("Numero mayor= {0} y numero menor = {1}",mayorMenor[0],mayorMenor[1]);
+
+            sucesionInv = f.Inversion(sucesion, sucesion.Length - 1, sucesionInv, 0);
+
+            Console.WriteLine("Sucesion inversa= ");
+            foreach (double a in sucesionInv)
+            {
+                Console.Write(a + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
